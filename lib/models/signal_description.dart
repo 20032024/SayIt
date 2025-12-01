22f0010b88b30cf36dd49ca159ea7fb093841a14
Ander Luna <zS23020596@estudiantes.uv.mx>
2025-12-01 04:41:37 -0600

    Update disign for camara and add detail for signs

---
+++ lib/models/signal_description.dart
@@ -0,0 +1,38 @@
+// signal_descriptions.dart
+
+final Map<String, Map<String, String>> signalDetails = {
+  "11": {
+    "title": "Right-of-way at intersection",
+    "meaning": "Indica quién tiene prioridad al cruzar una intersección.",
+    "description":
+        "Esta señal indica que el conductor tiene prioridad de paso sobre el tráfico "
+        "que se aproxima por la calle intersectante. Aun así, debe proceder con velocidad moderada y precaución.",
+  },
+  "12": {
+    "title": "Priority road",
+    "meaning": "Indica que la carretera donde circulas tiene prioridad.",
+    "description":
+        "Los vehículos que circulan en esta vía tienen preferencia en los próximos cruces, "
+        "a menos que exista otra señal que indique lo contrario.",
+  },
+  "14": {
+    "title": "Stop",
+    "meaning": "El conductor debe detenerse completamente.",
+    "description":
+        "El vehículo debe frenar totalmente antes de la línea o cruce, y avanzar solo "
+        "cuando esté seguro de que no hay peligro.",
+  },
+  "3": {
+    "title": "Speed limit (60km/h)",
+    "meaning": "Establece una velocidad máxima de 60 km/h.",
+    "description":
+        "Superar esta velocidad está prohibido. Se usa en zonas que requieren reducir riesgos "
+        "por tránsito frecuente o intersecciones cercanas.",
+  },
+  "8": {
+    "title": "Speed limit (120km/h)",
+    "meaning": "Establece una velocidad máxima de 120 km/h.",
+    "description":
+        "Generalmente usada en autopistas. Mantener el control y respetar distancias es obligatorio.",
+  },
+};
